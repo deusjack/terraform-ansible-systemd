0c6365b393af1174992d39490dc2f62076ec8547
Rasmus Pfeiffer <153379699+deusjack@users.noreply.github.com>
2024-09-07 15:12:21 +0200

    Fix count

--- modules/unit_files/outputs.tf
+++ modules/unit_files/outputs.tf
@@ -1,7 +1,7 @@
 output "triggers" {
   value = {
-    service = var.content.service != null ? module.service_file.triggers.playbook : ""
-    timer   = var.content.timer != null ? module.timer_file.triggers.playbook : ""
+    service = var.content.service != null ? one(module.service_file).triggers.playbook : ""
+    timer   = var.content.timer != null ? one(module.timer_file).triggers.playbook : ""
   }
   description = "Trigger for unit file changes to be used by other null_resources/modules"
 }
